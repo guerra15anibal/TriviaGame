338a513b29ab8e891d32380aaa987f889c59cd69
Anibal <guerra15anibal@hotmail.com>
2020-02-27 01:21:00 -0700

    adding questions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,43 @@
-var questions = []
+var myQuestions = [
+    {
+        questions: "What is Colorado's State animal?",
+        answers: {
+        a: "Rocky Moutian Goat",
+        b: "Big Foot",
+        c: "Ovis Canadensis",
+        d: "Columbine's"
+    },
+    correctAnswer: "c"
+},
+{
+    questions: "What is Colorado's State fish?",
+    answers: {
+    a: "Rainbow Trout",
+    b: "Great White Shark",
+    c: "Lake Trout",
+    d: "Greenback Cutthroat Trout"
+},
+    correctAnswer: "d" 
+},
+{
+    questions: "What is Colorado's State song?",
+    answers: {
+    a: "Where the Columbine's Grow",
+    b: "Oh Colorado!",
+    c: "I'ev Been Working on the Railroad",
+    d: "Baby Got Back"
+},
+    correctAnswer: "a" 
+}, 
+{
+    questions: "What is Colorado's State flower?",
+    answers: {
+    a: "Columbine Flower",
+    b: "Roses",
+    c: "SunFlower",
+    d: "Kush"
+},
+    correctAnswer: "d" 
+},
+
+]
