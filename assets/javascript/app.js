a081ffb609689a5eb4199824cdbccf8f12abf7dd
Anibal <guerra15anibal@hotmail.com>
2020-02-27 11:16:39 -0700

    adding radio buttons and timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,74 @@
-var myQuestions = [
-    {
-        questions: "What is Colorado's State animal?",
-        answers: {
-        a: "Rocky Moutian Goat",
-        b: "Big Foot",
-        c: "Ovis Canadensis",
-        d: "Columbine's"
-    },
-    correctAnswer: "c"
-},
-{
-    questions: "What is Colorado's State fish?",
-    answers: {
-    a: "Rainbow Trout",
-    b: "Great White Shark",
-    c: "Lake Trout",
-    d: "Greenback Cutthroat Trout"
-},
-    correctAnswer: "d" 
-},
-{
-    questions: "What is Colorado's State song?",
-    answers: {
-    a: "Where the Columbine's Grow",
-    b: "Oh Colorado!",
-    c: "I'ev Been Working on the Railroad",
-    d: "Baby Got Back"
-},
-    correctAnswer: "a" 
-}, 
-{
-    questions: "What is Colorado's State flower?",
-    answers: {
-    a: "Daisy",
-    b: "Columbine Flower",
-    c: "SunFlower",
-    d: "Kush"
-},
-    correctAnswer: "b" 
-},
-{
-    questions: "What is Colorado's State fruit?",
-    answers: {
-    a: "Green Apples",
-    b: "Grapes",
-    c: "Palisade Peach",
-    d: "Oranges"
-},
-    correctAnswer: "c" 
-},
-{
-    questions: "What is Colorado's most famous Boxer?",
-    answers: {
-    a: "Muhammad Ali",
-    b: "Rocky Marciano",
-    c: "Mike Tyson",
-    d: "Jack Dempsey"
-},
-    correctAnswer: "d" 
-}
+$(document).ready(function () {
 
-]
+    var myQuestions = [
+        {
+            questions: "What is Colorado's State animal?",
+            answers: {
+                a: "Rocky Moutian Goat",
+                b: "Big Foot",
+                c: "Ovis Canadensis",
+                d: "Moose"
+            },
+            correctAnswer: "c"
+        },
+        {
+            questions: "What is Colorado's State fish?",
+            answers: {
+                a: "Rainbow Trout",
+                b: "Great White Shark",
+                c: "Lake Trout",
+                d: "Greenback Cutthroat Trout"
+            },
+            correctAnswer: "d"
+        },
+        {
+            questions: "What is Colorado's State song?",
+            answers: {
+                a: "Where the Columbine's Grow",
+                b: "Oh Colorado!",
+                c: "I'ev Been Working on the Railroad",
+                d: "Baby Got Back"
+            },
+            correctAnswer: "a"
+        },
+        {
+            questions: "What is Colorado's State flower?",
+            answers: {
+                a: "Daisy",
+                b: "Columbine Flower",
+                c: "Sunflower",
+                d: "Kush"
+            },
+            correctAnswer: "b"
+        },
+        {
+            questions: "What is Colorado's State fruit?",
+            answers: {
+                a: "Green Apples",
+                b: "Grapes",
+                c: "Palisade Peach",
+                d: "Oranges"
+            },
+            correctAnswer: "c"
+        },
+        {
+            questions: "What is Colorado's most famous Boxer?",
+            answers: {
+                a: "Muhammad Ali",
+                b: "Rocky Marciano",
+                c: "Mike Tyson",
+                d: "Jack Dempsey"
+            },
+            correctAnswer: "d"
+        }
+    ];
+
+    // var seconds = 30, $seconds = document.querySelector(".timeLeft");
+    // (function countdown() {
+    //     $seconds.textContent = seconds + " time remaining";
+    //     if (seconds-- > 0) setTimeout(countdown, 1000);
+    //     else{
+    //         document.write("GAME OVER")
+    //     }
+    // })();
+});
